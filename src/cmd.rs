e846d814922d001160fb51690c73acb40ebb44ea
Ben-PH <benphawke@gmail.com>
2024-10-26 09:58:46 +0200

    Move handlers into dedicated fn

--- src/cmd.rs
+++ src/cmd.rs
@@ -2,12 +2,13 @@ use std::io::{self, ErrorKind};
 
 use camino::Utf8PathBuf;
 use clap::{Args, Parser, Subcommand};
-use tempdir::TempDir;
 
 use crate::flake::FlakeRefInput;
 
 mod parsers;
 mod stashed;
+/// Implementations for carrying out the various tasks
+mod handlers;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -16,7 +17,6 @@ pub struct Cli {
     pub command: SubCommand,
 }
 
-// TODO: split build and non-build commands
 #[derive(Subcommand, Debug)]
 pub enum SubCommand {
     Builders {
@@ -31,92 +31,37 @@ pub enum SubCommand {
     },
 }
 
-/// Commands that involve building: switch, test, boot, etc..
+/// Build-oriented tasks. See its `-h`/`--help` for more info.
 #[derive(Subcommand, Debug, strum::Display)]
 #[strum(serialize_all = "kebab-case")]
 pub enum BuildSubComms {
-    /// Build a config: Activate it, add it to boot-menu as default.
+    /// Build and... Activate it, add it to boot-menu as default.
     Switch,
-    /// Build a config, add it to boot-menu as default. Will not be activated.
+    /// Build and... Add it to boot-menu as default. Will not be activated.
     Boot,
-    /// Build and activate a config. Will not be added to boot-menu
+    /// Build and... Activate a config. Will not be added to boot-menu
     Test,
-    /// Build and sym-link only: No activation, or boot menu changes. Symlinks to configuration through `./result`.
+    /// Build and... Nothing. Makes sym-link to nix-store entry: `./result` by default
     ///
-    /// This is additional long-help
-    /// This should not be shown under `-h`
-    /// Only to be shown with `--help`
-    /// the short help will also be shown under `--help`
+    /// Use `--res-dir` to override default directory in which the `result` symlink will be placed
     Build,
     /// Build config only. Show (possibly incomplete) list of changes that its activation
     DryActivate,
     /// No-op, but shows the build/download ops performed by an actual build
     DryBuild,
+    /// Un-tested. Use at own risk. 
+    ///
+    /// See `nixos-rebuild --help` for what it would do if implemented
+    /// correctly
     BuildVm,
+    /// Un-tested. Use at own risk. 
+    ///
+    /// See `nixos-rebuild --help` for what it would do if implemented
+    /// correctly
     BuildVmWithBootloader,
 }
 
-impl BuildSubComms {
-    /// Builds a config, capturing a sym-link. Follows up with a call to `switch-to-configuration`
-    /// as appropriate.
-    pub fn run_build(&self, args: AllArgs) -> io::Result<()> {
-        log::trace!("Constructing configuration: {:?}", args);
-        let (res_dir, use_td) = self.build_configuration(args)?;
-
-        // Execute switch-to-configuration provided by the configuration build.
-        // This is where the switch/boot/test/dry-activate component gets carried out
-        if matches!(
-            self,
-            BuildSubComms::Switch
-                | BuildSubComms::Boot
-                | BuildSubComms::Test
-                | BuildSubComms::DryActivate
-        ) {
-            let switch_bin = res_dir.join("result/bin/switch-to-configuration");
-            let out_link = std::fs::canonicalize(switch_bin).unwrap();
-
-            let local_arch = std::env::var_os("LOCALE_ARCHIVE").unwrap_or_default();
-            let task_str = self.to_string();
-            let _ = cmd_lib::spawn!(
-                sudo nu -c "env -i LOCALE_ARCHIVE=$local_arch $out_link $task_str"
-            )?
-            .wait();
-        }
-
-        // Sanity-check that we are actually cleaning up a tempdir, and not nuking something that
-        // shouldn't be. This could justifyably be removed, as the OS GCs the tempdir anyway.
-        if use_td {
-            log::trace!("Cleaning up tempdir used to link to nix-store: {}", res_dir);
-            let sys_td = std::env::temp_dir();
-            assert!(std::fs::exists(&sys_td).unwrap());
-            assert!(res_dir.starts_with(sys_td));
-            assert!(
-                res_dir.file_name().unwrap().starts_with("nixrsbuild-"),
-                "{}",
-                res_dir.file_name().unwrap()
-            );
-            let _ = std::fs::remove_dir_all(res_dir);
-        }
-
-        Ok(())
-    }
-
-    /// Builds the configuration, and returns the link to the nix store repo. The `bool` tag
-    /// indicates if the link is placed in a temp-dir.
-    fn build_configuration(&self, args: AllArgs) -> io::Result<(Utf8PathBuf, bool)> {
-        let use_td = args.res_dir.is_none();
-        let full_flake = args.flake.init_flake_ref(self)?;
-        let res_dir = args.res_dir.unwrap_or(
-            Utf8PathBuf::from_path_buf(TempDir::new("nixrsbuild-")?.into_path()).unwrap(),
-        );
-        log::trace!("Result link directory: {}", res_dir);
-        full_flake
-            .run_nix_build(res_dir.as_path())
-            .map(|_| (res_dir, use_td))
-    }
-}
-
-/// Commands such as `repl`, `list-generations`, etc
+/// Tools-oriented tasks. See its `-h`/`--help` for more info.
 #[derive(Subcommand, Debug)]
 pub enum UtilSubCommand {
     ListGenerations {
---
+++ src/cmd/handlers.rs
@@ -0,0 +1,68 @@
+use std::io;
+
+use camino::Utf8PathBuf;
+use tempdir::TempDir;
+
+use super::AllArgs;
+
+
+impl super::BuildSubComms {
+    /// Builds a config, capturing a sym-link. Follows up with a call to `switch-to-configuration`
+    /// as appropriate.
+    pub fn run_build(&self, args: AllArgs) -> io::Result<()> {
+        log::trace!("Constructing configuration: {:?}", args);
+        let (res_dir, use_td) = self.build_configuration(args)?;
+
+        // Execute switch-to-configuration provided by the configuration build.
+        // This is where the switch/boot/test/dry-activate component gets carried out
+        if matches!(
+            self,
+            Self::Switch
+                | Self::Boot
+                | Self::Test
+                | Self::DryActivate
+        ) {
+            let switch_bin = res_dir.join("result/bin/switch-to-configuration");
+            let out_link = std::fs::canonicalize(switch_bin).unwrap();
+
+            let local_arch = std::env::var_os("LOCALE_ARCHIVE").unwrap_or_default();
+            let task_str = self.to_string();
+            let _ = cmd_lib::spawn!(
+                sudo nu -c "env -i LOCALE_ARCHIVE=$local_arch $out_link $task_str"
+            )?
+            .wait();
+        }
+
+        // Sanity-check that we are actually cleaning up a tempdir, and not nuking something that
+        // shouldn't be. This could justifyably be removed, as the OS GCs the tempdir anyway.
+        if use_td {
+            log::trace!("Cleaning up tempdir used to link to nix-store: {}", res_dir);
+            let sys_td = std::env::temp_dir();
+            assert!(std::fs::exists(&sys_td).unwrap());
+            assert!(res_dir.starts_with(sys_td));
+            assert!(
+                res_dir.file_name().unwrap().starts_with("nixrsbuild-"),
+                "{}",
+                res_dir.file_name().unwrap()
+            );
+            let _ = std::fs::remove_dir_all(res_dir);
+        }
+
+        Ok(())
+    }
+
+    /// Builds the configuration, and returns the link to the nix store repo. The `bool` tag
+    /// indicates if the link is placed in a temp-dir.
+    fn build_configuration(&self, args: AllArgs) -> io::Result<(Utf8PathBuf, bool)> {
+        let use_td = args.res_dir.is_none();
+        let full_flake = args.flake.init_flake_ref(self)?;
+        let res_dir = args.res_dir.unwrap_or(
+            Utf8PathBuf::from_path_buf(TempDir::new("nixrsbuild-")?.into_path()).unwrap(),
+        );
+        log::trace!("Result link directory: {}", res_dir);
+        full_flake
+            .run_nix_build(res_dir.as_path())
+            .map(|_| (res_dir, use_td))
+    }
+}
+
